01524971a9fc138256581599feb6a83734f6385a
ALMaclaine <almaclaine@gmail.com>
2019-10-12 16:30:43 -0700

    fix package.json

--- symbols.js
+++
@@ -1,7 +0,0 @@
-const allCapsAlpha = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-const allLowerAlpha = [...'abcdefghijklmnopqrstuvwxyz'];
-const allUniqueChars = [...'~!@#$%^&*()_+-=[]\{}|;:'\',./<>?'];
-const allNumbers = [...'0123456789'];
-const base = [...allCapsAlpha, ...allNumbers, ...allLowerAlpha, ...allUniqueChars];
-
-module.exports = base;
